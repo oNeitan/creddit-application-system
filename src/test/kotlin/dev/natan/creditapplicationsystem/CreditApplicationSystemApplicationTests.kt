101aa83d9dcd76603317a23bab55a44a75925b40
Natan Santos <natantech.sa@gmail.com>
2023-10-27 11:46:37 -0300

    starting the spring boot with Kotlin project

---
+++ src/test/kotlin/dev/natan/creditapplicationsystem/CreditApplicationSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.natan.creditapplicationsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditApplicationSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
